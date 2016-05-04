f2978fa621fc83f211e0c0a43a74b2170d3bdab1
messyhat <punkd1+1@gmail.com>
2016-05-04 17:51:46 +0800

    Add photo controller & views

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,5 +1,5 @@
 class HousesController < ApplicationController
-  before_action :set_room, only: [:show, :edit, :update]
+  before_action :set_house, only: [:show, :edit, :update]
   before_action :authenticate_user!, except: [:show]
 
   def index
@@ -7,7 +7,7 @@ class HousesController < ApplicationController
   end
 
   def show
-
+    @photos = @house.photos
   end
 
   def new
@@ -18,19 +18,39 @@ class HousesController < ApplicationController
     @house = current_user.houses.build(house_params)
 
     if @house.save
-      redirect_to @house, notice: "House created!"
+
+      if params[:images]
+        params[:images].each do |image|
+          @house.photos.create(image: image)
+        end
+      end
+
+      @photos = @house.photos
+      redirect_to edit_house_path(@house), notice: "Saved..."
     else
       render :new
     end
   end
 
   def edit
-
+    if current_user.id == @house.user.id
+      @photos = @house.photos
+  else
+    redirect_to root_path, notice: "You don't have permission to access that."
   end
+end
 
   def update
     if @house.update(house_params)
-      redirect_to @house, notice: "Updated House!"
+
+      if params[:images]
+        params[:images].each do |image|
+          @house.photos.create(image: image)
+        end
+      end
+
+      redirect_to edit_house_path(@house), notice: "House Updated!"
+
     else
       render :edit
     end
@@ -38,12 +58,18 @@ class HousesController < ApplicationController
 
 
   private
+
+
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
     def set_house
       @house = House.find(params[:id])
     end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def house_params
-      params.require(:house).permit(:home_type, :room_type, :bed_room, :bath_room, :listing_name, :address, :region, :price)
+      params.require(:house).permit(:home_type, :size, :summary, :fee_simple, :room_type, :bed_room, :isActive, :bath_room, :listing_name, :address, :region, :price)
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504015412) do
+ActiveRecord::Schema.define(version: 20160504054330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,18 @@ ActiveRecord::Schema.define(version: 20160504015412) do
   end
 
   add_index "houses", ["user_id"], name: "index_houses_on_user_id", using: :btree
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "house_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  add_index "photos", ["house_id"], name: "index_photos_on_house_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -65,4 +77,5 @@ ActiveRecord::Schema.define(version: 20160504015412) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "houses", "users"
+  add_foreign_key "photos", "houses"
 end
